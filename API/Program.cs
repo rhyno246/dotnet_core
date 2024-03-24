61e2729618db76a3176457a4e8d1a16b608c7c1d
mannguyen <nguyentrongminhman95@gmail.com>
2024-03-24 21:29:57 +0700

    updated

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,11 @@
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +20,9 @@ builder.Services.AddCors(opt => {
     });
 });
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
